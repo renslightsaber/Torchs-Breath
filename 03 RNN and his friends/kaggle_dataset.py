81558c6661bea151bf699869850cac322339777b
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-03-08 00:49:05 +0900

    data_path modified

--- 03 RNN and his friends/kaggle_dataset.py
+++ 03 RNN and his friends/kaggle_dataset.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 
-def kaggle_data_load(percent = 0.01, base_path =  './data/bitstampUSD_1-min_data_2012-01-01_to_2021-03-31.csv'):
+def kaggle_data_load(percent = 1, base_path =  './data/bitcoin-historical-data.csv'):
     df = pd.read_csv(base_path)
     df = df[df.Open.notnull()].reset_index(drop = True) # 결측치 제거
 
