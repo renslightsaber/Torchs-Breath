bcc390e35f09c2fd939ba7ab5b3449010796560f
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-03-09 23:53:06 +0900

    Update dataloader.py

--- 02 Classification/dataloader.py
+++ 02 Classification/dataloader.py
@@ -6,8 +6,8 @@ from torch.utils.data import Dataset, DataLoader
 
 class MyDataset(Dataset):
     def __init__(self, 
-                 data,  # = train_data.data,    # [50000, 3, 32, 32]
-                 label, # = train_data.targets  # [50000]
+                 data,  # = train_data.data,    
+                 label, # = train_data.targets 
                 ):
         self.x = torch.tensor( data / 255. , dtype = torch.float).permute(0, 3, 1, 2) 
         # self.x: Float + Shape Dimension 수정 + Torch Tensor로
@@ -24,28 +24,28 @@ class MyDataset(Dataset):
     
 
 
-def prepare_loaders(train_data, ratio = .6, bs = 128):
+def prepare_loaders(train_data, ratio, bs):
   
-  datas = train_data.data
-  labels = train_data.targets
-  index_num = int(datas.shape[0] * ratio)
-  
-  # train, valid split
-  train = datas[:index_num]
-  valid = datas[index_num:]
-  
-  train_label = labels[:index_num] # 30000 labels
-  valid_label = labels[index_num:] # 20000 labels
-  
-  # train_ds, valid_ds by MyDataset
-  train_ds = MyDataset(data = train, label = train_label)
-  valid_ds = MyDataset(data = valid, label = valid_label)
-  
-  # train_loader, valid_loader
-  # DataLoader에서 batch단위 크기로 row들을 묶어서 뱉어줌
-  train_loader = DataLoader(train_ds, batch_size = bs, shuffle = True)
-  valid_loader = DataLoader(valid_ds, batch_size = bs, shuffle = False)
-  
-  print("DataLoader Completed")
-  return train_loader, valid_loader
+    datas = train_data.data
+    labels = train_data.targets
+    index_num = int(datas.shape[0] * ratio)
+
+    # train, valid split
+    train = datas[:index_num]
+    valid = datas[index_num:]
+
+    train_label = labels[:index_num] # 30000 labels
+    valid_label = labels[index_num:] # 20000 labels
+
+    # train_ds, valid_ds by MyDataset
+    train_ds = MyDataset(data = train, label = train_label)
+    valid_ds = MyDataset(data = valid, label = valid_label)
+
+    # train_loader, valid_loader
+    # DataLoader에서 batch단위 크기로 row들을 묶어서 뱉어줌
+    train_loader = DataLoader(train_ds, batch_size = bs, shuffle = True)
+    valid_loader = DataLoader(valid_ds, batch_size = bs, shuffle = False)
+
+    print("DataLoader Completed")
+    return train_loader, valid_loader
 
